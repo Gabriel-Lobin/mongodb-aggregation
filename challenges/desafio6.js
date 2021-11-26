65c028c365de3c7ded3ecae4432d9442efd14bcc
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-11-26 18:26:25 -0300

    [requisito 6] lint

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -13,11 +13,11 @@ db.movies.aggregate([
   },
   {
     $project: {
-      _id:0,
+      _id: 0,
       maior_rating: 1,
       menor_rating: 1,
       media_rating: { $round: ["$media_rating", 1] },
-      desvio_padrao: { $round: ["$desvio_padrao" , 1] },
+      desvio_padrao: { $round: ["$desvio_padrao", 1] },
     },
   },  
 ]);
