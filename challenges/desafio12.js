e5cc3220692fa87722a1a304021ac3726d7b6570
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-11-26 20:37:09 -0300

    [requisito 12] resolucao

---
+++ challenges/desafio12.js
@@ -0,0 +1,31 @@
+db.trips.aggregate([
+  {
+    $addFields: {
+      diaDaSemana: { $dayOfWeek: "$startTime" },
+    },
+  },
+  {
+    $match: {
+      diaDaSemana: 5,   
+    },
+  },
+  {
+    $group: {
+      _id: "$startStationName",
+      soma: { $sum: 1 },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      nomeEstacao: "$_id",
+      total: "$soma",
+    },
+  }, 
+  {
+    $sort: { total: -1 },
+  },
+  {
+    $limit: 1,
+  },  
+]);
