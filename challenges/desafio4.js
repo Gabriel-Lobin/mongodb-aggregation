117069a5eb70dde55f9f8d74056ced587137428d
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-11-26 15:44:24 -0300

    [requisito 4] resolucao

---
+++ challenges/desafio4.js
@@ -0,0 +1,16 @@
+db.movies.aggregate([
+  {
+    $sort: { title: 1 },
+  },
+  {
+    $project: {
+      _id: 0,
+      title_split: { $split: ["$title", " "] },
+    },
+  },
+  {
+    $match: {
+      "title_split": { $size: 1 },
+    }
+  },
+]);
