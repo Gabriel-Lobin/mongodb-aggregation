614592460cad93cc787a1e7fbc1986db6a2d6d5a
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-11-27 12:00:55 -0300

    [requisito 13] resolucao

---
+++ challenges/desafio13.js
@@ -0,0 +1,47 @@
+db.trips.aggregate([
+  {
+    $addFields: {
+      verifiedDate: {
+        $dateToString: {
+          format: "%Y-%m-%d", date: "$startTime",
+        },
+      },
+    },
+  },
+  {
+    $match: {
+      verifiedDate: { $regex: /2016-03-10/ },
+    },
+  },
+  {
+    $project: {
+      usertype: 1,
+      sub: {
+        $subtract: ["$stopTime", "$startTime"],
+      },
+    },
+  },
+  {
+    $project: {
+      usertype: 1,
+      media: {
+        $divide: ["$sub",  60000],
+      },
+    },
+  },
+  {
+    $group: {
+      _id: "$verifiedDate",
+      duracaoMedia: { $avg: "$media" },
+    },
+  },  
+  {
+    $project: {
+      _id: 0,      
+      duracaoMediaEmMinutos: { $ceil: ["$duracaoMedia"] },
+    },
+  },
+  {
+    $sort: { duracaoMedia: 1 },
+  },
+]);
