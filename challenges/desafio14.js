1c9d94a9c7376d002b53a3a2d5edd7e2a7426973
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-11-27 12:10:57 -0300

    [requisito 14] resolucao

---
+++ challenges/desafio14.js
@@ -0,0 +1,37 @@
+db.trips.aggregate([
+  {
+    $project: {
+      bikeid: 1,
+      sub: {
+        $subtract: ["$stopTime", "$startTime"],
+      },
+    },
+  },
+  {
+    $project: {
+      bikeid: 1,
+      media: {
+        $divide: ["$sub", 60000 ],
+      },
+    },
+  },
+  {
+    $group: {
+      _id: "$bikeid",
+      duracaoMedia: { $avg: "$media" },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      tipo: "$_id",
+      duracaoMedia: { $ceil: ["$duracaoMedia"] },
+    },
+  },
+  {
+    $sort: { duracaoMedia: -1 },
+  },
+  {
+    $limit: 5,
+  },
+]);
